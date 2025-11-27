7ad5131b393a1bcad1bf9f71ac5805fbfaefb105
Marcos Allan <marcosallanm1to@gmail.com>
2025-11-27 14:39:41 -0300

    chore: update dependency name

--- src/main/java/dev/marcos/spring_security/controller/UserController.java
+++ src/main/java/dev/marcos/spring_security/controller/UserController.java
@@ -16,15 +16,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService authService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> save(@Valid @RequestBody UserRequestDTO dto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(authService.save(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(dto));
     }
 
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> findAll() {
-        return ResponseEntity.ok(authService.findAll());
+        return ResponseEntity.ok(userService.findAll());
     }
 }
